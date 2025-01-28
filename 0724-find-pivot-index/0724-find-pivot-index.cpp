13495f3dba7aa272385c94cf4a9f7ae4e631dd14
Nikhil Gupta <147332640+Nikhil8767@users.noreply.github.com>
2025-01-28 17:33:53 +0530

    Time: 0 ms (100%), Space: 35.8 MB (29.8%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,45 +2,23 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        int i=0,j=n-1;
-        int ans=-1;
-        while(i<j){
-            if(nums[i]<nums[j]){
-                i++;
-                nums[i]+=nums[i-1];
-            }
-            else if(nums[i]>nums[j]){
-                j--;
-                nums[j]+=nums[j+1];
 
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }  
+        int cs=0;
+        for(int i=0;i<n;i++){
+            int ls=cs;
+            int rs=sum-cs-nums[i];
+            
+            if(ls==rs){
+                return i;
             }
-            if(nums[i]==nums[j]){
-                ans=i;
-                i++;
-                j--;
+            else{
+                cs+=nums[i];
             }
         }
-        if(i==n-1) return -1;
-        else if(ans==0) return 0;
-        else return ans+1;
-
-
-
-        // for(int i=1;i<n;i++){
-        //     nums[i]+=nums[i-1];
-        // }
-        // int ans=-1;
-        // for(int i=0;i<n;i++){
-        //     if(nums[n-1]-nums[i]==(nums[n-1]/2)-1){
-        //         ans= i;
-        //     }
-        //     else if(nums[n-1]-nums[i]<0){
-        //         ans= 0;
-        //     }
-
-        // }
-        // return ans;
-        
-        
+        return -1;
     }
 };
