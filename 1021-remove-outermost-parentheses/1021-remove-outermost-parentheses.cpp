28c6e271cd61cbbac76e1bd8275d8dea96766c29
Nikhil Gupta <147332640+Nikhil8767@users.noreply.github.com>
2025-02-06 18:55:02 +0530

    Time: 0 ms (100%), Space: 9 MB (55.46%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n=s.length();
+        string result="";
+        int depth=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                if(depth>0){result+=s[i];}
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth>0){
+                    result+=s[i];
+                }
+            }
+        }
+        return result;
+    }
+};
