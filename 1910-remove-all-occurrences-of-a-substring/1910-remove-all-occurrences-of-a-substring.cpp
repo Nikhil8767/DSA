d9f2b43d0a648b053b803b42537b96d8ada94ab7
Nikhil Gupta <147332640+Nikhil8767@users.noreply.github.com>
2025-02-11 17:34:56 +0530

    Time: 3 ms (32.43%), Space: 12 MB (9.01%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+        string result="";
+        int n=part.length();
 
-        while(s.find(part) !=string::npos){
-            s.erase(s.find(part),part.length());
+        for(char c:s){
+            result.push_back(c);
+
+            if(result.length()>=n && result.substr(result.length()-n)==part){
+                result.erase(result.length()-n);
+            }
         }
-        return s;
+        return result;
+
+
+
+
+        // this is my first approch now lets do by the stack one the above one
+        // while(s.find(part) !=string::npos){
+        //     s.erase(s.find(part),part.length());
+        // }
+        // return s;
         
     }
 };
